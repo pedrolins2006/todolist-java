92d3f6713a240e47e360ab97c5c2090a027588d8
phlins2006 <phlins2006@gmail.com>
2025-06-04 08:27:05 -0400

    config inicial do projeto

---
+++ backend/src/main/java/com/todolist/todoslist_api/TodoslistApiApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.todoslist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoslistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoslistApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/todolist/todoslist_api/TodoslistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.todoslist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoslistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
